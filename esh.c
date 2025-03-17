b46757705322afe9022fcbad21c1694d84ed59b4
Roc <1361050885@qq.com>
2025-03-18 00:44:36 +0800

    2025 spring

---
+++ esh.c
@@ -0,0 +1,61 @@
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <sys/ptrace.h>
+#include <stdarg.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <signal.h>
+
+//
+// You should use the following functions to print information
+// Do not modify these functions
+//
+
+void print_prompt() {
+    printf("esh > ");
+    fflush(stdout);
+}
+
+void print_invalid_syntax() {
+    printf("Invalid Syntax\n");
+    fflush(stdout);
+}
+
+void print_command_not_found() {
+    printf("Command Not Found\n");
+    fflush(stdout);
+}
+
+void print_execution_error() {
+    printf("Execution Error\n");
+    fflush(stdout);
+}
+
+void print_blocked_syscall(char* syscall_name, int count, ...) {
+    va_list args;
+    va_start(args, count);
+    printf("Blocked Syscall: %s ", syscall_name);
+    for (int i = 0; i < count; i++) {
+        char* arg = va_arg(args, char*);
+        printf("%s ", arg);
+    }
+    printf("\n");
+    fflush(stdout);
+}
+
+// 
+// You can add your own functions here
+//
+
+int main() {
+    while(1) {
+        print_prompt();
+        // TODO
+        // Show your intelligence
+        break;
+    }
+}
