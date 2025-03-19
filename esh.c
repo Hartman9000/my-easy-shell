fc7d7e5ee13fca911f8c61b8a7f7001c97486115
Suo Zhenhai <govlandshal@outlook.com>
2025-03-19 10:17:16 +0800

    apply tokenize

--- esh.c
+++ esh.c
@@ -15,6 +15,9 @@
 // Do not modify these functions
 //
 
+#define MAX_TOKENS 50
+#define MAX_PATH_LEN 50
+
 void print_prompt() {
     printf("esh > ");
     fflush(stdout);
@@ -51,9 +54,49 @@ void print_blocked_syscall(char* syscall_name, int count, ...) {
 // You can add your own functions here
 //
 
+int tokenize(char *prompt, char *tokens[], int max_tokens) {
+    int count = 0;
+    char *token = strtok(prompt, " ");
+
+    while (token != NULL && count < max_tokens) {
+        tokens[count++] = token;
+        token = strtok(NULL, " ");
+    }
+    
+    return count;
+}
+
+char *tokens[MAX_TOKENS];
+
 int main() {
+    char cwd[MAX_PATH_LEN];
+    getcwd(cwd, sizeof(cwd));
+    setenv("PATH", "/bin", 1);
+    setenv("HOME", cwd, 1);
+    setenv("PWD", cwd, 1);
+    setenv("OLDPWD", cwd, 1);
+    setenv("LANG", "en_US.UTF-8", 1);
+    setenv("ESH_VERSION", "alpha", 1);
+
     while(1) {
         print_prompt();
+
+        char *row_prompt = NULL;
+        size_t prompt_buffer_size = 0;
+        ssize_t read = getline(&row_prompt, &prompt_buffer_size, stdin);
+        row_prompt[read - 1] = '\0';
         
+        int token_count = tokenize(row_prompt, tokens, MAX_TOKENS);
+        printf("总共有 %d 个 token:\n", token_count);
+        for (int i = 0; i < token_count; i++) {
+            printf("tokens[%d] = \"%s\"\n", i, tokens[i]);
+        }
+        if (strcmp(tokens[0], "exit") == 0) {
+            exit(0);
+        }
+        
+        free(row_prompt);
+
+        // break;
     }
 }
